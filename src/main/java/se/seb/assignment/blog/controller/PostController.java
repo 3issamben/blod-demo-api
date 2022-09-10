90f26313de3afe340eaea2e4cd0ac7c4328c2852
Issam Ben <issam.ben@quickbit.com>
2022-09-10 18:30:28 +0200

    Add blog post controller

---
+++ src/main/java/se/seb/assignment/blog/controller/PostController.java
@@ -0,0 +1,38 @@
+package se.seb.assignment.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.seb.assignment.blog.model.Post;
+import se.seb.assignment.blog.service.PostService;
+
+import java.util.List;
+
+@RequestMapping("api/blog")
+@RestController
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    // Add new blog entry
+    @PostMapping
+    public ResponseEntity<Post> savePost(@RequestBody Post post){
+        return new ResponseEntity<Post>(postService.savePost(post), HttpStatus.CREATED);
+    }
+
+    // Get all blog entries
+    @GetMapping
+    public List<Post> getAllPosts(){
+        return postService.getAllPosts();
+    }
+
+    // Delete post by ID
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deletePost(@PathVariable("id") long id){
+        postService.deletePost(id);
+        return new ResponseEntity<String>("Post deleted successfully", HttpStatus.OK);
+    }
+
+}
