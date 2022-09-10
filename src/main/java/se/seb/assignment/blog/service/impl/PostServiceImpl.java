bff9bed9034e53d9881c7b077b592a09995cf39b
Issam Ben <issam.ben@quickbit.com>
2022-09-10 18:29:55 +0200

    Add post service implementation

---
+++ src/main/java/se/seb/assignment/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,37 @@
+package se.seb.assignment.blog.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import se.seb.assignment.blog.model.Post;
+import se.seb.assignment.blog.repository.PostRepository;
+import se.seb.assignment.blog.service.PostService;
+
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private final PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository){
+        super();
+        this.postRepository = postRepository;
+    }
+
+
+    @Override
+    public Post savePost(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public void deletePost(long id) {
+        postRepository.deleteById(id);
+    }
+}
