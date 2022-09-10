87644063297281ba2fe45cd3093070d2d9db9355
Issam Ben <issam.ben@quickbit.com>
2022-09-10 22:18:28 +0200

    Add tag model

---
+++ src/main/java/se/seb/assignment/blog/model/Tag.java
@@ -0,0 +1,41 @@
+package se.seb.assignment.blog.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Tag(){
+
+    }
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
